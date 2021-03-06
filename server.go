0c9b977c75dea2e9e0c20ea71396c2558b0c1579
Darunee (IT.Appl.DM) <darunee@corp.jaymart.co.th>
2021-03-06 15:19:36 +0700

    Ok

--- server.go
+++ server.go
@@ -9,10 +9,33 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
+type Todo struct {
+	ID int  `json:"id"`
+	Title  string `json:"title"`
+	Status  string `json:"status"`
+
+
+}
+
+var todos = map[int]*Todo{
+	1: &Todo{ID:1 , Title:"I am Darunee", Status:"active"},
+}
+
 func helloHandler(c echo.Context) error {
 	return c.JSON(http.StatusOK, map[string]string{
 		"message": "hello Darunee",
 	})
+
+}
+
+
+func getTodosHandler(c echo.Context) error {
+	   items := []*Todo{}
+	   for _,item := rang todos{
+          items = append(items, item)
+
+	   }
+	   return c.JSON(http.StatusOK,items)
 
 }
 
@@ -22,6 +45,7 @@ func main() {
 	e.Use(middleware.Recover())
 
 	e.GET("/hello", helloHandler)
+	e.GET("/todos", getTodosHandler)
 	port := os.Getenv("PORT")
 	log.Println("port:", port)
 	e.Start(":" + port)
