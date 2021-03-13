6f2931959231ac361463f7e416dcf1dc1a8b4fdb
Darunee (IT.Appl.DM) <darunee@corp.jaymart.co.th>
2021-03-13 14:43:15 +0700

    test api

--- server.go
+++ server.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	_ "github.com/lib/pq"
 )
 
 type Todo struct {
@@ -36,6 +38,59 @@ func getTodosHandler(c echo.Context) error {
 
 }
 
+func createTodosHandler(e echo.Context) error {
+	t := Todo{}
+
+	if err := e.Bind(&t); err != nil {
+		return e.JSON(http.StatusBadRequest, map[string]string{"error": err.Error()})
+
+	}
+	id := len(todos)
+	id++
+	t.ID = id
+	todos[t.ID] = &t
+	return e.JSON(http.StatusCreated, "created todo.")
+
+}
+
+func getTodoByIDHandler(c echo.Context) error {
+	var id int
+	//var title, status string
+	err := echo.PathParamsBinder(c).Int("id", &id).BindError()
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err)
+	}
+
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal("Connect to database error", err)
+	}
+	defer db.Close()
+
+	stmt, err := db.Prepare("SELECT id, title, status FROM todos where id=$1")
+	if err != nil {
+		log.Fatal("can'tprepare query one row statment", err)
+	}
+
+	row := stmt.QueryRow(id)
+	//var id int
+	var title, status string
+
+	err = row.Scan(&id, &title, &status)
+	if err != nil {
+		log.Fatal("can't Scan row into variables", err)
+	}
+
+	t := Todo{
+		ID:     id,
+		Title:  title,
+		Status: status,
+	}
+
+	return c.JSON(http.StatusOK, t)
+
+}
+
 func main() {
 	e := echo.New()
 	e.Use(middleware.Logger())
@@ -43,6 +98,9 @@ func main() {
 
 	e.GET("/hello", helloHandler)
 	e.GET("/todos", getTodosHandler)
+	e.GET("/todos/:id", getTodoByIDHandler)
+	e.POST("/todos", createTodosHandler)
+
 	port := os.Getenv("PORT")
 	log.Println("port:", port)
 	e.Start(":" + port)
